0348292be1ddd1316a81d2e05b6957cd8011e7a5
ryohei-iwamoto <celeron5576@gmail.com>
2024-07-30 22:34:05 +0900

    applicationクラスにnotfound処理を追加

--- core/Application.php
+++ core/Application.php
@@ -90,4 +90,73 @@ abstract class Application
     {
         return $this->getRootDir() . '/web';
     }
+
+    public function run()
+    {
+        try {
+            $params = $this->router->resolve($this->request->getPathInfo());
+            if ($params === false) {
+                throw new HttpNotFoundException('No route found for' . $this->request->getPathInfo);
+            }
+            $controller = $params['controller'];
+            $action = $params['action'];
+
+            $this->runAction($controller, $action, $params);
+        } catch (HttpNotFoundException $e) {
+            $this->render404page($e);
+        }
+
+        $this->response->send();
+    }
+
+    public function runAction($controller_name, $action, $params=array())
+    {
+        $controller_class = ucfirst($controller_name) . "Controller";
+        if ($controller_name===false){
+            throw new HttpNotFoundException($controller_class . " controller is not found.");
+        }
+
+        $content = $controller->run($action, $params);
+        $this->response->setContent($content);
+    }
+
+    protected function findController($controller_class)
+    {
+        if (!class_exists($controller_class)) {
+            $controller_file = $this->getControllerDir() . "/" . $controller_class . '.php';
+        }
+
+        if (!is_readable($controller_file)){
+            return false;
+        } else {
+            require_once $controller_file;
+
+            if(!class_exists($controller_class)){
+                return false;
+            }
+        }
+        return new $controller_class($this);
+    }
+
+    protected function render404page($e)
+    {
+        $this->response->setStatusCode(404, "Not Found");
+        $message = $this->isDebugMode() ? $e->getMessage() : "Page Not Found.";
+        $message = htmlspecialchars($message, ENT_AUOTES, "UTF-8");
+
+        $this->response->setContent(<<<EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+    <title>404</title>
+   </head>
+<body>
+    {$message}
+</body>
+</html>
+EOF
+        );
+    }
 }
