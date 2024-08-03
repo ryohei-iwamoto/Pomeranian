f54324983975df289d0eef594a8ca391fc36b354
ryohei-iwamoto <celeron5576@gmail.com>
2024-08-03 16:49:00 +0900

    controllerの実装

---
+++ core/Controller.php
@@ -0,0 +1,73 @@
+<?php
+
+abstract class Controller
+{
+    protected $controller_name;
+    protected $action_name;
+    protected $application;
+    protected $request;
+    protected $response;
+    protected $session;
+    protected $db_manager;
+
+    public function __construct($application)
+    {
+        $this->controller_name = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application  = $application;
+        $this->request      = $application->getRequest();
+        $this->response     = $application->getResponse();
+        $this->session      = $application->getSession();
+        $this->db_manager   = $application->getDbManager();
+    }
+
+    public function run($action, $params=array())
+    {
+        $this->action_name = $action;
+
+        $action_method = $action . "Action";
+        if (!method_exists($this, $action_method)){
+            $this->forword404();
+        }
+        return $this->$action_method($params);
+    }
+
+    protected function render($variables=array(), $template_name=null, $layout="layout")
+    {
+        $default_vals = array(
+            "request"   => $this->request,
+            "base_url"  => $this->request->getBaseUrl(),
+            "session"   => $this->session,
+        );
+
+        $view = new View($this->application->getViewDir(), $default_vals);
+
+        if (is_null($template_name)){
+            $template_name = $this->action_name;
+        }
+
+        $path = $this->controller_name . "/" . $template_name;
+
+        return $view->render($path, $variables, $layout);
+    }
+
+    protected function forward404()
+    {
+        throw new HttpNotFoundException(
+            "Forwarded 404page from {$this->controller_name} / {$this->action_name}");
+    }
+
+    protected function redirect($url)
+    {
+        if(!preg_match('#https?://#', $url)){
+            $protocol   = $this->request->isSsl() ? "https://" : "http://";
+            $host       = $this->request->getHost();
+            $base_url   = $this->request->getBaseUrl();
+
+            $url = $protocol . $host . $base_url . $url;
+        }
+
+        $this->response->setStatusCode(302, 'Found');
+        $this->response->setHttpHeader('Location', $url);
+    }
+}
